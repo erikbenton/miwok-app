3d6444e506d507b900158a74395797be60c29ffe
erikbenton <erik.benton@hotmail.com>
2018-03-28 15:23:09 -0400

    using for loop to add textviews to numbers screen

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,37 +18,16 @@ public class NumbersActivity extends AppCompatActivity {
 
         LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
 
-        TextView wordView1  = new TextView(this);
-        TextView wordView2  = new TextView(this);
-        TextView wordView3  = new TextView(this);
-        TextView wordView4  = new TextView(this);
-        TextView wordView5  = new TextView(this);
-        TextView wordView6  = new TextView(this);
-        TextView wordView7  = new TextView(this);
-        TextView wordView8  = new TextView(this);
-        TextView wordView9  = new TextView(this);
-        TextView wordView10 = new TextView(this);
+        // Adding TextView for each entry in words
+        for(int i = 0; i < words.size(); i++)
+        {
+            //Creating TextView
+            TextView wordView = new TextView(this);
+            //Setting text to be an entry in words
+            wordView.setText(words.get(i));
+            //Adding that view to the rootView
+            rootView.addView(wordView);
+        }
 
-        wordView1.setText(words.get(0));
-        wordView2.setText(words.get(1));
-        wordView3.setText(words.get(2));
-        wordView4.setText(words.get(3));
-        wordView5.setText(words.get(4));
-        wordView6.setText(words.get(5));
-        wordView7.setText(words.get(6));
-        wordView8.setText(words.get(7));
-        wordView9.setText(words.get(8));
-        wordView10.setText(words.get(9));
-
-        rootView.addView(wordView1);
-        rootView.addView(wordView2);
-        rootView.addView(wordView3);
-        rootView.addView(wordView4);
-        rootView.addView(wordView5);
-        rootView.addView(wordView6);
-        rootView.addView(wordView7);
-        rootView.addView(wordView8);
-        rootView.addView(wordView9);
-        rootView.addView(wordView10);
     }
 }

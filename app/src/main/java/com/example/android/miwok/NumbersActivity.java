84e49d866c6dea0003d16f77671031803b591902
erikbenton <erik.benton@hotmail.com>
2018-03-29 11:07:05 -0400

    filling in rest of the categories for miwok/english translations

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,22 +2,16 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
         ArrayList<Word> words = new ArrayList<Word>();
 
         words.add(new Word("one", "lutti"));
@@ -31,11 +25,11 @@ public class NumbersActivity extends AppCompatActivity {
         words.add(new Word("nine", "wo'e"));
         words.add(new Word("ten", "na'aacha"));
 
-        WordAdapter itemsAdapter = new WordAdapter(this, words);
+        WordAdapter adapter = new WordAdapter(this, words);
 
         ListView listView = (ListView) findViewById(R.id.list);
 
-        listView.setAdapter(itemsAdapter);
+        listView.setAdapter(adapter);
 
     }
 }
